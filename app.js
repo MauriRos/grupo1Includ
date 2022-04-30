a72d9fc43406f147f35c1758cb929b3f4c8c5761
unknown <rostag.mj@gmail.com>
2022-04-30 11:18:39 -0300

    modificaciones en app.js y en home por rosty

--- app.js
+++ app.js
@@ -15,3 +15,15 @@ app.listen(3000, () => {
  app.get ('/', (req,res) => {
      res.sendFile(path.join(__dirname, "./views/home.html"))
  });
+
+ app.get ('/', (req,res) => {
+    res.sendFile(path.join(__dirname, "./views/loginRegister.html"))
+});
+
+app.get ('/', (req,res) => {
+    res.sendFile(path.join(__dirname, "./views/productCart.html"))
+});
+
+app.get ('/', (req,res) => {
+    res.sendFile(path.join(__dirname, "./views/productDetail.html"))
+});
