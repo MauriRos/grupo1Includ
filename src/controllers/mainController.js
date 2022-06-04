86722257c520cad63c5845088281c77a6a35e450
Juan Belbey <belbeyjuan@gmail.com>
2022-06-04 18:57:25 -0300

    probando logica del home

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,6 +1,9 @@
 const path = require('path');
 let ejs = require(('ejs'));
+const fs = require('fs');
 
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 let productsTop = [
     {
@@ -90,6 +93,9 @@ const mainController = {
     home: (req,res) => {
         res.render('home', { productsTop : productsTop, productsAccesorios: productsAccesorios, productsPantalones: productsPantalones})
         },
+    home2: (req,res) => {
+        res.render ('home2', {products: products})
+    }
 };
 
     
