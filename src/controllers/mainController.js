111ca806c338b16336c6f4a91146b5f45aaca593
MauriRos <rostagno.mj@gmail.com>
2022-07-30 18:20:30 -0300

    limpieza codigo

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -6,9 +6,7 @@ const db = require("../../database/models");
 
 
 const mainController = {
-    // home: (req,res) => {
-    //     res.render ('home', {products: products})
-    // },
+ 
     home: (req,res) => {
         db.Product.findAll()
         .then(products => {
@@ -16,12 +14,7 @@ const mainController = {
             res.render("home", {products})
         });
         
-    }
-
-    
-    // home2: (req,res) => {
-    //     res.render ('home2', {products: products})
-    // }
+    }  
 };
 
     
--- src/routes/users.js
+++ src/routes/users.js
@@ -36,8 +36,8 @@ router.get("/loginRegister", guestMiddleware, userController.loginRegister);
 
 router.post("/register",  uploadFile.any(), validateRegisterForm,userController.register)
 router.post("/login", validacionesLogin, userController.login);
-router.get("/check", userController.check);
 router.get("/logOut", userController.logOut);
+router.get('/forbidden', userController.forbidden);
 
 
 module.exports = router ;
